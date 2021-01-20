d94da0b2c8f36ea80d58ead406ee483fad9fdece
sarazhan <67008676+sarazhan@users.noreply.github.com>
2021-01-20 08:46:32 +0100

    Update 20-01_funzione.py

--- 20-01_funzione.py
+++ 20-01_funzione.py
@@ -1,12 +1,12 @@
 def fuoco_para(a, b, c):
     fx = (1-b**2)+(4*a*c)
     fy = -b/(2*a)
-    print("Fuoco =", fx, fy)
+    return fx, fy
 
 def vertice_para(a,b,c):
     vx = -b/(2*a)
     vy = (4*a*c-b**2)/(4*a)
-    print("Vertice =", vx, vy)
+    return vx, vy
 
 a = int(input("Quanto vale a? "))
 b = int(input("Quanto vale b? "))
@@ -14,3 +14,4 @@ c = int(input("Quanto vale c? "))
 
 Fuoco = fuoco_para(a, b, c)
 Vertice = vertice_para(a,b,c)
+print("Fuoco",Fuoco," Vertice", Vertice)
