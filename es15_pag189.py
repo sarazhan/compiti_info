33e3873a6e4d58f5d1543b07e5dc8fa31f36a162
sarazhan <67008676+sarazhan@users.noreply.github.com>
2021-01-28 07:21:41 +0100

    aggiunto_esercizi

--- es15_pag189.py
+++ es15_pag189.py
@@ -1,3 +1,4 @@
+# Consegna 28/01/21: Dato un elenco di nazioni e uno di capitali, visualizza la capitale di una nazione fornita dalla tastiera o un messaggio di errore.
 def geografia():
     nazione = input("Inserisci nazioni in ordine, separati da uno spazio: ").split()
     capitali = input("Inserisci capitali in ordine, separali con uno spazio: ").split()
